d0928ca177517feaa37936b83353ab518521f9f2
Hao Zhang <zhanhao@microsoft.com>
2023-04-29 10:24:52 +0800

    Initial commit: Copied Sample Code from ElectronNET

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ElectronNET.API;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ElectronNET.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) => { logging.AddConsole(); })
+                .UseElectron(args)
+                .UseStartup<Startup>();
+        }
+    }
+}
